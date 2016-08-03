788e5702b9ae90c941bb8bc75b86cbfc22bf7a9e
Jie Pang <pangjiefb@gmail.com>
2016-08-02 21:50:02 -0400

    regular commit

--- js/controller.js
+++ js/controller.js
@@ -3,17 +3,22 @@
 angular
 .module('jpangApp')
 
-.controller("NavCtrl", ["$scope",function($scope) {
+.controller("NavCtrl", ["$scope", function($scope) {
 	// $scope.navbarCollapsed = true;
 }])
 
+.controller('SkillCtrl', ['$scope', function ($scope) {
+
+}])
+
 .controller('ScrollCtrl', ['$scope', '$location', 'anchorSmoothScroll', function($scope, $location, anchorSmoothScroll) {
-    $scope.gotoElement = function (eID){
-      // set the location.hash to the id of
-      // the element you wish to scroll to.
-      $location.hash('bottom');
-      // call $anchorScroll()
-      anchorSmoothScroll.scrollTo(eID);
-    };
+	$scope.gotoElement = function (eID){
+	  // set the location.hash to the id of
+	  // the element you wish to scroll to.
+	  $location.hash('bottom');
+	  // call $anchorScroll()
+	  anchorSmoothScroll.scrollTo(eID);
+	};
 }])
 ;
+
