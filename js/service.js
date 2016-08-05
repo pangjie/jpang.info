225dd5d456ee9d877b5817897cab2f4b2e8ec6f6
Jie Pang <pangjiefb@gmail.com>
2016-08-05 14:49:01 -0400

    regular commit

--- js/service.js
+++ js/service.js
@@ -13,7 +13,7 @@ angular
 			scrollTo(0, stopY); return;
 		}
 		var speed = Math.round(distance / 100);
-		if (speed >= 20) speed = 20;
+		if (speed >= 30) speed = 30;
 		var step = Math.round(distance / 25);
 		var leapY = stopY > startY ? startY + step : startY - step;
 		var timer = 0;
